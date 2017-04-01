00b54d003329add40df723e6c2a5f53db48d544b
Maxfield Walker <maxfield.walker@gmail.com>
2017-04-01 10:54:05 +0900

    cosme

--- test/parser/parser.test.ts
+++ test/parser/parser.test.ts
@@ -7,51 +7,19 @@ import {
 } from "../../src/parser/parser";
 import * as util from "util";
 import * as fs from "fs";
+import * as assert from "assert";
+import * as _ from "lodash";
 
 
 suite("Parser", () => {
-    test("operand parser", () => {
-        const p = operandP();
-        const r = p.parse("GR1");
-        console.log(r);
-    });
-
-    test("literal parser", () => {
-        const p = literalP();
-        const r = p.parse("=123");
-        console.log(r);
-    });
-
-    test("instruction line parser", () => {
-        const p = instructionLineP();
-
-        const lines = [
-            "CASL START   "
-        ];
-
-        for (const l of lines) {
-            const r = p.parse(l);
-            if (r.status) {
-                console.log(printAST(r.value));
-            }
-        }
-    });
-
-    test("comment line parser", () => {
-        const p = commentLineP();
-
-        const r1 = p.parse("   ");
-        const r2 = p.parse("   ;hello");
-        const r3 = p.parse("");
-        console.log(r1);
-
-    });
-
     test("parse source", () => {
-        const path = "./test/baseline/basic/simple.cas";
+        const path = "./test/baseline/basic/all.cas";
         const source = fs.readFileSync(path).toString();
         const parser = new Parser();
         const result = parser.parseSource(path, source);
-        fs.writeFileSync("./AST.json", printAST(result.sourceFile));
+
+        const actual = printAST(result.sourceFile);
+        const expected = fs.readFileSync("./test/baseline/basic/all.AST.json").toString();
+        assert.equal(actual, expected);
     });
 });
