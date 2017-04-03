61a40d4aacae984e66f9b9ecc8ed66d84c04813f
Maxfield Walker <maxfield.walker@gmail.com>
2017-04-03 22:41:55 +0900

    make line ending of testdata \r\c

--- test/parser/parser.test.ts
+++ test/parser/parser.test.ts
@@ -5,6 +5,7 @@ import {
     grP, labelP, literalP, instructionLineP, operandP, syntaxKindToStr,
     commentLineP, Parser, printAST
 } from "../../src/parser/parser";
+import * as os from "os";
 import * as util from "util";
 import * as fs from "fs";
 import * as assert from "assert";
@@ -14,7 +15,9 @@ import * as _ from "lodash";
 suite("Parser", () => {
     test("parse source", () => {
         const path = "./test/baseline/basic/all.cas";
-        const source = fs.readFileSync(path).toString();
+        // 改行コードが異なるとパース結果が変わってしまうので
+        // 改行コードを\r\nに揃える
+        const source = fs.readFileSync(path).toString().replace(new RegExp(os.EOL, "g"), "\r\n");
         const parser = new Parser();
         const result = parser.parseSource(path, source);
 
